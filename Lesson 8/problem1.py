45adbd9353b573b3dc221356f5301fe003c76a47
JaniahPayne <44148033+JaniahPayne@users.noreply.github.com>
2018-12-17 12:36:41 -0500

    Add files via upload

---
+++ Lesson 8/problem1.py
@@ -0,0 +1,21 @@
+print('_'* 79)
+
+age = input ('what is your age?')
+
+age = int(age)
+
+result = 79 - age 
+
+year = result + 2018 
+	
+	Print('...thinking...')
+
+	Print('...thinking...')
+
+	Print('...thinking...')
+
+	Print('...thinking...')
+
+	print('You will die in the year ' + str(year))
+
+	print ( '_'* 79)
